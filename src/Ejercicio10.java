1818e3e5f119c2218171c3086c0086a96ce72001
Agustin Fernandez <agusfdez.01@gmail.com>
2026-03-24 17:06:59 -0300

    Ejercicio 10 Finalizado

---
+++ src/Ejercicio10.java
@@ -0,0 +1,17 @@
+void main() {
+    double[] saldos = new double[10];
+    saldos[0] = 912.18;
+    saldos[1] = 912.18;
+    saldos[2] = 912.18;
+    saldos[3] = 912.18;
+    saldos[4] = 912.18;
+    saldos[5] = 912.18;
+    saldos[6] = 912.18;
+    saldos[7] = 912.18;
+    saldos[8] = 912.18;
+    saldos[9] = 912.18;
+    System.out.println("El saldo de la decima cuenta es: " + saldos[9]);
+
+    double[] saldos2 = {91218.31, 91218.31, 91218.31, 91218.31, 91218.31, 91218.31, 91218.31, 91218.31, 91218.31, 91218.31, };
+    System.out.println("\nEl saldo de la tercera cuenta es: " + saldos2[2]);
+}
