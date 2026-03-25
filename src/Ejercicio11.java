d55ccf854b8082ea700d7305551929ad68f31a57
Agustin Fernandez <agusfdez.01@gmail.com>
2026-03-24 22:29:42 -0300

    Ejercicio 11 Finalizado

---
+++ src/Ejercicio11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+void main() {
+    Scanner teclado = new Scanner(System.in);
+    String[] asignaturas = {"Matematicas", "Fisisca", "Quimica", "Historia", "Lengua"};
+    double[] notas = new double[asignaturas.length];
+    for (int i = 0; i < asignaturas.length; i++) {
+        System.out.print("Ingrese la nota de " + asignaturas[i] +": \n");
+        double nota = teclado.nextDouble();
+        notas[i] = nota;
+    }
+    System.out.println("\n--- BOLETÍN DE CALIFICACIONES ---");
+    for (int i = 0; i < asignaturas.length; i++) {
+        System.out.println("La nota de " + asignaturas[i] + "es : " + notas[i]);
+    }
+}
